ed233f79fc69d5419ad196406e545d4db88fffa0
Delsoc Ibrahim <d.ibrahim@cse-it.net>
2022-11-12 17:30:48 +0100

    cutKuevetten.m kommentiert

--- cutKuevetten.m
+++ cutKuevetten.m
@@ -1,9 +1,18 @@
 function [croppedImages] = cutKuevetten(RGB, stats)
+% Die Funktion cutKuevetten erwartet als Eingabe ein Bild, sowie die mit
+% regionprops ermittelten stats/Rahmenbegrenzungen der einzelnen gefundenen
+% Objekte in dem Bild. Diese Objekte werden dann aus dem Bild geschnitten
+% und in der Sammluing "croppedImages" gespeichert.
+
+    % es wird über jedes gefundene Objekt iteriert
     for k = 1 : length(stats)
+
+        % Die Rahmenbegrenzungen des aktuellen Objekts werden ausgelesen
+        % und zwischengespeichert
         BB = stats(k).BoundingBox;
-        %rectangle('Position', [BB(1),BB(2),BB(3),BB(4)],'EdgeColor','r','LineWidth',2);
-        %figure;
+        
+        % Das Objekt wird aus dem Bild geschnitten und croppedImages
+        % angehangen
         croppedImages{k} = imcrop(RGB, [BB(1) BB(2) BB(3) BB(4)]);
-        %imshow(croppedImages{k});
     end
 end
