0d143a6d0322c85df95c7e56a7fbc9a0a2fed889
Delsoc Ibrahim <d.ibrahim@cse-it.net>
2022-11-12 20:37:55 +0100

    safeHeights.m kommentiert

--- safeHeights.m
+++ safeHeights.m
@@ -1,18 +1,23 @@
 function safeHeights(heightPixelHoughAlgo,heightPixelRedPlaneAlgo,heightPixelSaettigungsAlgo, imageIndex, kuevetteIndex, completeHeight)
-    %The position of all components required for photography are fixed at
-    %all times! the length of 1 pixel was evaluated: 93px = 7mm = 7000µm.
-    %1px = 0,075mm = 75000µm
+    % Diese Funktion bekommt als Eingabe die detektierten Höhen der Linie
+    % der aktuellen Küvette. Außerdem wird der Bildindex, der Küvettenindex,
+    % sowie die Gesamthöhe der Küvette als Eingabe erwartet. Diese Funktion
+    % speichert die ermittelten Höhen und die komplette Höhe der Küvette in
+    % jeweils eine Matrix
 
+    % Die Matrizen, in denen die Werte gespeichert werden, werden geladen
     completeKuevettenHeight = matfile('completeKuevettenHeight.mat').completeKuevettenHeight;
     detectedLineHeightMatrixHough = matfile('detectedLineHeightMatrixHough.mat').detectedLineHeightMatrixHough;
     detectedLineHeightMatrixRedPlane = matfile('detectedLineHeightMatrixRedPlane.mat').detectedLineHeightMatrixRedPlane;
     detectedLineHeightMatrixSaettigung = matfile('detectedLineHeightMatrixSaettigung.mat').detectedLineHeightMatrixSaettigung;
 
+    % Die Werte werden in den jeweiligen Matrizen gespeichert
     completeKuevettenHeight(imageIndex,kuevetteIndex) = completeHeight;
     detectedLineHeightMatrixHough(imageIndex,kuevetteIndex) = heightPixelHoughAlgo;
     detectedLineHeightMatrixRedPlane(imageIndex,kuevetteIndex) = heightPixelRedPlaneAlgo;
     detectedLineHeightMatrixSaettigung(imageIndex,kuevetteIndex) = heightPixelSaettigungsAlgo;
     
+    % Die Matrizen werden gespeichert.
     save('completeKuevettenHeight.mat','completeKuevettenHeight');
     save('detectedLineHeightMatrixHough.mat','detectedLineHeightMatrixHough');
     save('detectedLineHeightMatrixRedPlane.mat','detectedLineHeightMatrixRedPlane');
